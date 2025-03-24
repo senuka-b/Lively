c0a3eceecbe7fb677a16dbba306a76e18a97b9dc
yetimeh <senukabandara2006@gmail.com>
2025-03-24 21:56:30 +0530

    Create websocket configuration and other configuration files. Update IceCandidate to include Candidate type

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,3 +3,8 @@
 pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
 R
 "src/main/java/org/senuka/Main.java,4\c\4c092362052cc69b074bbe7848e64788cb64c7c5
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+]
+-src/main/java/org/senuka/confg/AppConfig.java,3\0\30545a8a696aae90a75631817227e094188df613
--- src/main/java/org/senuka/dto/IceCandidate.java
+++ src/main/java/org/senuka/dto/IceCandidate.java
@@ -1,13 +1,19 @@
 package org.senuka.dto;
 
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.Builder;
 import lombok.Data;
+import org.senuka.util.IceCandidateType;
 
 @Data
 @Builder
 public class IceCandidate {
     private Long id;
     private String candidateData;
+
+    @Enumerated(EnumType.STRING)
+    private IceCandidateType type;
 
     private Call call;
 }
